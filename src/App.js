b31b656df7fcf6d68b253b77e66213aa1a88f3e9
Jaden Padua <jadenpadua@Jadens-MacBook-Pro.local>
2020-08-29 15:36:48 -0700

    Hello

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-     <h1>Hi, this is a React App</h1>
+     <h1>Hi, this is a React Apps</h1>
       </div>
     );
   }
